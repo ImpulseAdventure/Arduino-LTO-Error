714736434c466475a22159baee22bc30f23fa24c
Calvin Hass <ImpulseAdventure@users.noreply.github.com>
2018-12-12 22:25:06 -0800

    Create MyLib.c

---
+++ test-lto/MyLib.c
@@ -0,0 +1,8 @@
+// This sketch & accompanying library were written to demonstrate the avr-gcc LTO compiler error
+// Author: Calvin Hass (https://github.com/ImpulseAdventure/Arduino-LTO-Error)
+
+#include "MyLib.h"
+
+void myFunc(tsBig* pBig)
+{
+}
